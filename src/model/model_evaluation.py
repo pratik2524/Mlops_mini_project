3988d8061981e60734189ceb53281c457efb7266
Pratik <pratikmahadik2524@gmail.com>
2024-11-09 14:58:25 +0530

    Added token based auth for dagshub

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -11,8 +11,20 @@ import mlflow.sklearn
 import dagshub
 import os
 
-mlflow.set_tracking_uri('https://dagshub.com/pratik2524/Mlops_mini_project.mlflow')
-dagshub.init(repo_owner='pratik2524', repo_name='Mlops_mini_project', mlflow=True)
+#setup Dagshub for MLFLOW tracking
+
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "pratik2524"
+repo_name = "Mlops_mini_project"
+
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # logging configuration
 logger = logging.getLogger('model_evaluation')
--- src/model/model_registration.py
+++ src/model/model_registration.py
@@ -6,9 +6,18 @@ import logging
 import os
 import dagshub
 
-mlflow.set_tracking_uri('https://dagshub.com/pratik2524/Mlops_mini_project.mlflow')
-dagshub.init(repo_owner='pratik2524', repo_name='Mlops_mini_project', mlflow=True)
+dagshub_token = os.getenv("DAGSHUB_PAT")
+if not dagshub_token:
+    raise EnvironmentError("DAGSHUB_PAT environment variable is not set")
 
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "pratik2524"
+repo_name = "Mlops_mini_project"
+
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 
 # logging configuration
 logger = logging.getLogger('model_registration')
